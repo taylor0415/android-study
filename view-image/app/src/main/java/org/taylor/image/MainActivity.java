a63cc5f9958420eb754fdc4082ad075ccbb9f3b4
taylor0415 <taylor0415@163.com>
2018-01-23 22:26:56 +0800

    新增缓存机制
    新增缓存机制

--- view-image/app/src/main/java/org/taylor/image/MainActivity.java
+++ view-image/app/src/main/java/org/taylor/image/MainActivity.java
@@ -6,10 +6,13 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -35,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
 
         imageView = findViewById(R.id.image);
         editText = findViewById(R.id.url);
+        System.out.println();
     }
 
     public void viewImage(View view) {
@@ -44,25 +48,40 @@ public class MainActivity extends AppCompatActivity {
 
                 String urlStr = editText.getText().toString();
 
-                try {
-                    URL url = new URL(urlStr);
+                File file=new File(getCacheDir(), Base64.encodeToString(urlStr.getBytes(),Base64.DEFAULT));
+                if(file.exists() && file.length()>0){
+                    System.out.println("使用缓存图片...");
+                }else {
+                    System.out.println("第一次加载图片...");
+                    try {
+                        URL url = new URL(urlStr);
 
-                    HttpURLConnection urlConnection = (HttpURLConnection)url.openConnection();
-                    urlConnection.setRequestMethod("GET");
-                    urlConnection.setConnectTimeout(5000);
-                    int code=urlConnection.getResponseCode();
+                        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
 
-                    if (code==200) {
-                        InputStream inputStream = urlConnection.getInputStream();
-                        Bitmap bitmap= BitmapFactory.decodeStream(inputStream);
-                        Message msg=Message.obtain();
-                        msg.obj=bitmap;
-                        handler.sendMessage(msg);
+                        FileOutputStream fos = new FileOutputStream(file);
+                        urlConnection.setRequestMethod("GET");
+                        urlConnection.setConnectTimeout(5000);
+                        int code = urlConnection.getResponseCode();
+
+                        if (code == 200) {
+                            InputStream inputStream = urlConnection.getInputStream();
+                            int len = -1;
+                            byte[] buffer = new byte[1024];
+                            while ((len = inputStream.read(buffer)) != -1) {
+                                fos.write(buffer, 0, len);
+                            }
+                            fos.close();
+                            inputStream.close();
+                        }
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
                     }
-
-                } catch (Exception e) {
-                    e.printStackTrace();
                 }
+                Bitmap bitmap = BitmapFactory.decodeFile(file.getAbsolutePath());
+                Message msg = Message.obtain();
+                msg.obj = bitmap;
+                handler.sendMessage(msg);
             }
         }.start();
     }
